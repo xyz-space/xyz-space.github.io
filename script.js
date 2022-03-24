17529bfea79a151666c07c66fa594416d17c789d
Raphael Lepuschitz <raphael.lepuschitz@chello.at>
2022-03-24 15:46:12 +0100

    Website Cleanup & Bugfixes

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ var links = {
     bio: "<a href='https://lockdowncommute.space'>Website</a>",
     type: "<a href='Lockdown.html'>Performance</a>"
   },
-  Red: {
+  SeeingRed: {
     year: "<a href='SeeingRed.html'>2020</a>",
     name: "<a href='SeeingRed.html'>Seeing Red</a>",
     collaborator: "<a href='SeeingRed.html'>Paloma Moniz, Ricardo Gonçalves</a>",
     bio: "<a href='SeeingRed.html'>Work in Progress</a>",
     type: "<a href='SeeingRed.html'>Website</a>"
   },
-  Nik: {
+  NiklasStoehr: {
     year: "<a href='NiklasStoehr.html'>2020</a>",
     name: "<a href='NiklasStoehr.html'>Niklas Stoehr</a>",
     collaborator: "<a href='NiklasStoehr.html'>—</a>",
     bio: "<a href='https://niklas-stoehr.com'>Website</a>",
     type: "<a href='NiklasStoehr.html'>Website</a>"
   },
-  ALine: {
+  ALWFAV3: {
     year: "<a href='ALWFAV3.html'>2019</a>",
     name: "<a href='ALWFAV3.html'>ALWFAV3</a>",
     collaborator:
       "<a href='ALWFAV3.html'>Paloma Moniz, Ricardo Gonçalves, Jaime Del Corro, Margarida Morais, Francisca Roseiro</a>",
     type: "<a href='ALWFAV3.html'>Publication, Symposium</a>"
   },
-  Copy: {
+  TheCopyMachine: {
     year: "<a href='TheCopyMachine.html'>2019</a>",
     name: "<a href='TheCopyMachine.html'>The Copy Machine</a>",
     collaborator: "<a href='TheCopyMachine.html'>—</a>",
@@ -41,13 +41,13 @@ var links = {
     collaborator: "<a href='Nature.html'>—</a>",
     type: "<a href='Nature.html'>AI, Publication, AR</a>"
   },
-  Nostalgia: {
-    year: "<a href='Synthetic.html'>2019</a>",
-    name: "<a href='Synthetic.html'>Synthetic Nostalgia</a>",
-    collaborator: "<a href='Synthetic.html'>CMP Office</a>",
-    type: "<a href='Synthetic.html'>AI, Publication, Installation</a>"
+  SyntheticNostalgia: {
+    year: "<a href='SyntheticNostalgia.html'>2019</a>",
+    name: "<a href='SyntheticNostalgia.html'>Synthetic Nostalgia</a>",
+    collaborator: "<a href='SyntheticNostalgia.html'>CMP Office</a>",
+    type: "<a href='SyntheticNostalgia.html'>AI, Publication, Installation</a>"
   },
-  Hustler: {
+  PremiumHustler: {
     year: "<a href='PremiumHustler.html'>2019</a>",
     name: "<a href='PremiumHustler.html'>Premium Hustler</a>",
     collaborator: "<a href='PremiumHustler.html'>Paloma Moniz, Ricardo Gonçalves</a>",
@@ -71,7 +71,6 @@ var links = {
     collaborator: "<a href='Rundgang.html'>—</a>",
     type: "<a href='Rundgang.html'>Poster</a>"
   },
-
   Transparency: {
     year: "<a href='Transparency.html'>2016</a>",
     name: "<a href='Transparency.html'>Transparency</a>",
@@ -118,44 +117,45 @@ $(document).ready(function (event) {
 //  h = $(window).height();
 //});
 
-$("#container").on("mouseenter", ".line", function () {
-  let i = $(this).index() - 2;
-  console.log(i);
-  // select image images[i]
-  $("body").css("background-image", `url(${images[i]})`);
-});
-
-$("#container").on("mouseleave", ".line", function () {
-  let i = -1;
-  console.log(i);
-  $("body").css("background-image", "");
-});
-
-$("#clockContainer").click(function () {
-  $(".about").toggleClass("about-alt");
-  $("#th").toggleClass("th-alt");
-  $("#linklist").toggleClass("linklist-alt");
-  $(".line").toggleClass("line-alt");
-  $(".year").toggleClass("year-alt");
-  $(".title").toggleClass("title-alt");
-  $(".with").toggleClass("with-alt");
-  $(".collaborator").toggleClass("collaborator-alt");
-  $(".type").toggleClass("type-alt");
-  $(".bio").toggleClass("bio-alt");
-});
-
-$("#container").on("mouseenter", ".collaborator", function () {
-  if (window.location == window.parent.location) {
-    $(".bio").removeClass("bio-show");
-    $(this).find(".bio").addClass("bio-show");
-  }
-});
-
-$("#container").on("mouseleave", ".line", function () {
-  $(".bio").removeClass("bio-show");
-});
-
 function makeLinks() {
+
+  $("#container").on("mouseenter", ".line", function () {
+    let i = $(this).index() - 2;
+    //console.log(i);
+    // select image images[i]
+    $("body").css("background-image", `url("img/${images[i]}")`);
+  });
+
+  $("#container").on("mouseleave", ".line", function () {
+    let i = -1;
+    //console.log(i);
+    $("body").css("background-image", "");
+  });
+
+  $("#clockContainer").click(function () {
+    $(".about").toggleClass("about-alt");
+    $("#th").toggleClass("th-alt");
+    $("#linklist").toggleClass("linklist-alt");
+    $(".line").toggleClass("line-alt");
+    $(".year").toggleClass("year-alt");
+    $(".title").toggleClass("title-alt");
+    $(".with").toggleClass("with-alt");
+    $(".collaborator").toggleClass("collaborator-alt");
+    $(".type").toggleClass("type-alt");
+    $(".bio").toggleClass("bio-alt");
+  });
+
+  $("#container").on("mouseenter", ".collaborator", function () {
+    if (window.location == window.parent.location) {
+      $(".bio").removeClass("bio-show");
+      $(this).find(".bio").addClass("bio-show");
+    }
+  });
+
+  $("#container").on("mouseleave", ".line", function () {
+    $(".bio").removeClass("bio-show");
+  });
+
   for (var [key, value] of Object.entries(links)) {
     let title = value.name;
     let collaborator = value.collaborator;
@@ -164,10 +164,10 @@ function makeLinks() {
     let type = value.type;
 
     let newline = $(
-      `<a class='line' href='${linkBody + key}' target='_top'></a>`
+      `<a class='line' href='${linkBody + key}.html' target='_top'></a>`
     );
     newline.append($(`<span class="year">${year}</span>`));
-    newline.append($(`<span class="title"><a ">${title}</a></span>`));
+    newline.append($(`<span class="title">${title}</span>`));
     newline.append($(`<span class="with">with</span>`));
     let collaboratorSpan = $(`<span class="collaborator">${collaborator}</span>`);
     if (bio) {
